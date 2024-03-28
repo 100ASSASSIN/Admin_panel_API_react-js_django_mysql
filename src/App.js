54abd63b97ca8354d403bb0628cda686bf0a5dc5
ASSASSIN <tamilthasan11@gamil.com>
2024-03-28 20:20:42 +0530

    sds

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const Home = lazy(() => import('./Pages/Home'));
 const About = lazy(() => import('./Pages/About'));
 const NoMatch = lazy(() => import('./Components/NoMatch'));
 const Assassin = lazy(() => import('./Pages/assassin'));
+const View = lazy(() => import('./view_panel/view_panel'));
 
 const App = () => {
 	return (
@@ -15,6 +16,7 @@ const App = () => {
 					<Route path="/" element={<Home />} />
 					<Route path="/about" element={<About />} />
 					<Route path="/assassin" element={<Assassin />} />
+					<Route path="/panel" element={<View />} />
 					<Route path="*" element={<NoMatch />} />
 				</Routes>
 			</Suspense>
---
+++ src/view_panel/view_panel.jsx
@@ -0,0 +1,9 @@
+import React  from "react";
+
+function View(){
+    return(
+        <div id=''>
+        </div>
+    );
+}
+export default View;
