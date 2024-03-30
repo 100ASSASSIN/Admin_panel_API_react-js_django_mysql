151ad4ad9f560b21c59cff6df6e49123db9f049e
ASSASSIN <tamilthasan11@gamil.com>
2024-03-31 00:39:05 +0530

    dsds

--- src/view_panel/view_panel.jsx
+++ src/view_panel/view_panel.jsx
@@ -16,11 +16,12 @@ function View(){
     return(
         <div id='panel'>
             <h3>{g}</h3>
-            <h3>{text}</h3>
+            <h3 style={{color:'yellow'}}> {text} </h3>
             <h1>test</h1>
             <div id="img"></div>
-            <Gamer />
-            <Add />
+            <div style={{color:'bule'}}><Gamer /></div>
+            <div style={{color:'red'}}><Add /></div>
+            <Table />
         </div>
     );
 }
@@ -35,4 +36,12 @@ const Gamer = () => {
     ); 
 }
 Gamer();
+const Table = () => {
+    return(
+        <div>
+            <p>Updates to your homepage feed
+We've combined the power of the Following feed with the For you feed so there’s one place to discover content on GitHub. There’s improved filtering so you can customize your feed exactly how you like it, and a shiny new visual design. ✨</p>
+        </div>
+    );
+}
 export default View;
