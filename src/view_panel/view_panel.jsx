bd71c70fa51b5174a143ea48bf1303d6269b48ee
ASSASSIN <tamilthasan11@gamil.com>
2024-04-10 19:07:15 +0530

    dsds

--- src/view_panel/view_panel.jsx
+++ src/view_panel/view_panel.jsx
@@ -3,36 +3,6 @@ import { Helmet } from "react-helmet";
 import { useCookies } from 'react-cookie';
 import '../Components/nav.css';
 import { Link } from 'react-router-dom';
-function ApiData() {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    // Fetch data from the API
-    fetch('http://localhost:8000/api/api/')
-      .then(response => response.json())
-      .then(data => {
-        // Extracting the message array from the API response
-        const messages = data[0].message;
-        setData(messages);
-      })
-      .catch(error => {
-        console.error('Error fetching data:', error);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>API Data</h1>
-      <ul>
-        {data.map((item, index) => (
-          <li key={index}>
-            ID: {item[0]}, Name: {item[1]}, Password: {item[2]}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
 
 function View() {
   const [cookies, setCookies, removeCookies] = useCookies(); // Add setCookies
@@ -52,9 +22,10 @@ function View() {
     <div id='panel'>
       <div id="Dashborad"></div>
       <New />
-      <ApiData />
       <button id='but' onClick={handleLogout} style={{ padding: '10px 20px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Logout</button>{/* Add logout button */}
       <Link to="/Users"><div id="Users">List of users</div></Link>
+      <Link to="/Users"><div id="items">list of Items</div></Link>
+      <Link to="/Users"><div id="orders">Orders</div></Link>
     </div>
   );
 }
