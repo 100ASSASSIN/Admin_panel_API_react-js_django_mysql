906c19a013cfea544024f5b3de58ab5986ecd749
ASSASSIN <tamilthasan11@gamil.com>
2024-04-10 18:56:59 +0530

    dsd

--- src/view_panel/user.jsx
+++ src/view_panel/user.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { Helmet } from "react-helmet";
+import { useCookies } from 'react-cookie';
 
 const Users = () => {
+    const [cookies] = useCookies(['user']);
+
+    useEffect(() => {
+        checkUser();
+    }, []); // Added empty dependency array
+
+    function checkUser() {
+        if (cookies.user) {
+            // User is logged in
+            console.log("User is logged in:", cookies.user);
+        } else {
+            // Redirect to '/assassin' if user is not logged in
+            window.location.replace("/assassin");
+        }
+    }
+
     return (
         <div>
-            test
+            <Helmet>
+                <title>Users</title>
+            </Helmet>
+            <h1>Users Page</h1>
         </div>
     );
 }
