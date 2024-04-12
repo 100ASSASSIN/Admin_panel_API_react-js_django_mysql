98b6b92d384c73ae356baa0aefee39261a2824e8
ASSASSIN <tamilthasan11@gamil.com>
2024-04-13 00:02:28 +0530

    dssd

--- src/view_panel/profi.jsx
+++ src/view_panel/profi.jsx
@@ -16,9 +16,8 @@ function Profi() {
       window.location.replace("/")
     };
     return (<div>
-          
-         <div>
-          <div id="pro"></div> <div id="name"><button id='but2' onClick={handleLogout} style={{ padding: '10px 40px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer' }}>Logout</button>{/* Add logout button */}</div>
+         <div>  
+          <div id="pro"></div> <div id="name"> <button  onClick={handleLogout} style={{ padding: '10px 40px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', cursor: 'pointer', position:'absolute', left:"0px" }}>Logout</button>{/* Add logout button */}</div>
          <div id='pro2' style={{color: '#007bff',paddingTop:'300px', paddingLeft:'250px'}}>© 2017-2024 Copyright ASSASSIN UNIVERSAL STUDIOS. Terms of Use · Privacy Policy .</div>
          </div>
     </div>
